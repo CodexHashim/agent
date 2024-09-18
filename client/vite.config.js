64b34ade9ff4f72cd57027cdbe48baac5171f2d9
Hashim Ahmed <55762512+CodexHashim@users.noreply.github.com>
2024-09-18 19:39:39 +0530

    hope so all fixed issue it finally ir

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html', // This is usually not necessary if index.html is in the root
+      },
+    },
+  },
+});
